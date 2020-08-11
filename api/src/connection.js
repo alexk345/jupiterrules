8266c196030bfa1b86a0952b39c96c79b4071bb6
alexk345 <alexk345@Gmail.com>
2020-08-10 20:51:58 -0700

    test

--- api/src/connection.js
+++ api/src/connection.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 const User = require("./User.model");
 
 mongoose.Promise = global.Promise;
+ 
+ 
+const connection = "mongodb://srv-captain--mongo:27017/mongo-test";	
 
-
-const connection = "mongodb://root:#Test345@srv-captain--mongo:27017/mongo-test";
-
-const connectDb = () => {
-  return mongoose.connect(connection,{ useNewUrlParser: true });
+const connectDb = () => {	
+  return mongoose.connect(connection,{ useNewUrlParser: true });	  
 };
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected')
+});
+
 module.exports = connectDb;
- 
